646349b898d6e84682a365a876fcd8dec06158d2
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-11 05:25:10 +0100

    day 6

---
+++ Day 06/main.swift
@@ -0,0 +1,17 @@
+import AoC_Helpers
+
+let ages = input().dropLast().split(separator: ",").asInts()
+
+var byTimer = (0...8).map { ages.count(of: $0) }
+func simulate() {
+	let reproducing = byTimer[0]
+	byTimer.removeFirst()
+	byTimer[6] += reproducing
+	byTimer.append(reproducing)
+}
+
+for _ in 1...80 { simulate() }
+print("after 80 days:", byTimer.reduce(0, +))
+
+for _ in 81...256 { simulate() }
+print("after 256 days:", byTimer.reduce(0, +))
