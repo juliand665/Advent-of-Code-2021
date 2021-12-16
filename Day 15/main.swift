7947f202ae7925e274597aaa809c88eed78b6712
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-16 14:46:30 +0100

    faster computations

--- Day 15/main.swift
+++ Day 15/main.swift
@@ -4,31 +4,36 @@ import Collections
 
 let riskLevels = Matrix(digitsOf: input().lines())
 
-func pathRisks(in riskLevels: Matrix<Int>) -> Matrix<Int> {
-	Matrix(
-		width: riskLevels.width,
-		height: riskLevels.height,
-		repeating: Int.max
-	) <- { pathRisks in
-		pathRisks[.zero] = 0
-		var toSearch: Deque = [Vector2.zero]
-		while let start = toSearch.popFirst() {
-			for neighbor in start.neighbors where riskLevels.isInMatrix(neighbor) {
-				let cost = pathRisks[start] + riskLevels[neighbor]
-				if cost < pathRisks[neighbor] {
-					pathRisks[neighbor] = cost
-					toSearch.append(neighbor)
-				}
-			}
-		}
+struct Candidate: Comparable {
+	var position: Vector2
+	var cost: Int
+	
+	static func < (lhs: Self, rhs: Self) -> Bool {
+		lhs.cost < rhs.cost
 	}
 }
 
-let smallPathRisks = pathRisks(in: riskLevels)
-print("lowest-risk path to end in small version:", smallPathRisks.elements.last!)
+func pathRiskToEnd(in riskLevels: Matrix<Int>) -> Int {
+	let target = Vector2(riskLevels.width - 1, riskLevels.height - 1)
+	var toSearch: Heap = [Candidate(position: .zero, cost: 0)]
+	var searched = Set<Vector2>()
+	while let start = toSearch.popMin() {
+		guard !searched.contains(start.position) else { continue }
+		searched.insert(start.position)
+		for neighbor in start.position.neighbors where riskLevels.isInMatrix(neighbor) {
+			let cost = start.cost + riskLevels[neighbor]
+			guard neighbor != target else { return cost }
+			toSearch.insert(Candidate(position: neighbor, cost: cost))
+		}
+	}
+	fatalError("target not reached!")
+}
+
+let smallPathRisk = pathRiskToEnd(in: riskLevels)
+print("lowest-risk path to end in small version:", smallPathRisk)
 
 let fullMap = Matrix(width: 5, height: 5) { pos in
 	riskLevels.map { ($0 + pos.absolute - 1) % 9 + 1 }
 }.flattened()
-let fullPathRisks = pathRisks(in: fullMap)
-print("lowest-risk path to end in full version:", fullPathRisks.elements.last!)
+let fullPathRisk = pathRiskToEnd(in: fullMap)
+print("lowest-risk path to end in full version:", fullPathRisk)
