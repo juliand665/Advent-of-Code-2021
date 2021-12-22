0e22c17050c3cfc68ac74c8224df6532ba9d8e16
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-22 23:08:01 +0100

    day 22

--- Day 22/main.swift
+++ Day 22/main.swift
@@ -3,8 +3,10 @@ import SimpleParser
 import HandyOperators
 
 extension Range {
-	func split(at split: Bound) -> [Range] {
-		contains(split) ? [lowerBound..<split, split..<upperBound] : [self]
+	func intersection(with other: Self) -> Self? {
+		let lower = Swift.max(lowerBound, other.lowerBound)
+		let upper = Swift.min(upperBound, other.upperBound)
+		return lower < upper ? lower..<upper : nil
 	}
 }
 
@@ -15,50 +17,13 @@ struct Cuboid {
 		x.count * y.count * z.count
 	}
 	
-	var lowerBound: Vector3 {
-		.init(x.lowerBound, y.lowerBound, z.lowerBound)
-	}
-	
-	var upperBound: Vector3 {
-		.init(x.upperBound, y.upperBound, z.upperBound)
-	}
-	
-	func allPositions() -> [Vector3] {
-		x.flatMap { x in
-			y.flatMap { y in
-				z.map { z in
-					Vector3(x, y, z)
-				}
-			}
-		}
-	}
-	
-	func contains(_ position: Vector3) -> Bool {
-		x.contains(position.x) && y.contains(position.y) && z.contains(position.z)
-	}
-	
-	func split(at split: Vector3) -> [Self] {
-		x.split(at: split.x).flatMap { x in
-			y.split(at: split.y).flatMap { y in
-				z.split(at: split.z).map { z in
-					Cuboid(x: x, y: y, z: z)
-				}
-			}
-		}
-	}
-	
-	private func split(atBoundsOf other: Self) -> [Self] {
-		split(at: other.lowerBound)
-			.flatMap { $0.split(at: other.upperBound) }
-	}
-	
-	func overlaps(with other: Self) -> Bool {
-		x.overlaps(other.x) && y.overlaps(other.y) && z.overlaps(other.z)
-	}
-	
-	func subtracting(_ other: Self) -> [Self] {
-		split(atBoundsOf: other)
-			.filter { !other.overlaps(with: $0) }
+	func intersection(with other: Self) -> Self? {
+		guard
+			let x = x.intersection(with: other.x),
+			let y = y.intersection(with: other.y),
+			let z = z.intersection(with: other.z)
+		else { return nil }
+		return .init(x: x, y: y, z: z)
 	}
 }
 
@@ -108,34 +73,33 @@ extension Step: Parseable {
 }
 
 struct State {
-	var cuboids: [Cuboid] = []
+	var regions: [(cuboid: Cuboid, adjustment: Int)] = []
 	
 	var cubeCount: Int {
-		cuboids.map(\.volume).sum()
+		regions.map { $0.volume * $1 }.sum()
 	}
 	
-	func applying(_ step: Step) -> Self {
-		.init(
-			cuboids: cuboids
-				.flatMap { $0.subtracting(step.cuboid) }
-				+ (step.state ? [step.cuboid] : [])
-		)
+	mutating func apply(_ step: Step) {
+		regions += regions
+			.lazy
+			.compactMap { cuboid, adjustment in
+				step.cuboid
+					.intersection(with: cuboid)
+					.map { ($0, -adjustment) }
+			}
+		if step.state {
+			regions.append((step.cuboid, 1))
+		}
 	}
 }
 
 func state(after steps: [Step]) -> State {
-	steps.enumerated().reduce(.init()) { state, step in
-		let (index, step) = step
-		print("applying step \(index)/\(steps.count)")
-		return state.applying(step)
-	}
+	steps.reduce(into: .init()) { $0.apply($1) }
 }
 
 let steps = input().lines().map(Step.init)
-
 let initSteps = steps.prefix(while: \.isForInitialization)
 
-print("starting...")
 let afterInit = state(after: Array(initSteps))
 print("after initialization, \(afterInit.cubeCount) cubes are on")
 
