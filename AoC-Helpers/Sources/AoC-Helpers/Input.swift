1e0c6499568f7ffd95bbed7b4c908b2b641018c9
Julian Dunskus <julian.dunskus@gmail.com>
2021-12-26 21:24:19 +0100

    better trailing newline handling

--- AoC-Helpers/Sources/AoC-Helpers/Input.swift
+++ AoC-Helpers/Sources/AoC-Helpers/Input.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-public func input(filename: String = "input") -> String {
+public func input(filename: String = "input") -> Substring {
 	let url = URL(fileURLWithPath: Bundle.main.path(forResource: filename, ofType: "txt")!)
 	let rawInput = try! Data(contentsOf: url)
-	return String(data: rawInput, encoding: .utf8)!
+	return String(data: rawInput, encoding: .utf8)!.dropLast() // trailing newline
 }
 
 extension StringProtocol {
 	public func lines() -> [SubSequence] {
-		split(separator: "\n", omittingEmptySubsequences: false).dropLast()
+		split(separator: "\n", omittingEmptySubsequences: false)
 	}
 	
 	public func words() -> [SubSequence] {
